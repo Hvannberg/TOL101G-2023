a904641248c1d600c0e732a649844b626a3538f8
Ebba Þóra Hvannberg <ebba@hi.is>
2023-08-23 21:50:50 +0000

    fyrsta

---
+++ Vika2/Plus.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *
+ *  Lýsing  : Leggur saman tvo fasta og prentar út niðurstöðu
+ *
+ *
+ *****************************************************************************/
+
+public class Plus {
+    public static void main(String[] args) {
+        final int A = 2;
+        final int B = 4;
+        System.out.print(A);
+        System.out.print(" + ");
+        System.out.print(B);
+        System.out.print(" eru ");
+        System.out.println(A + B);
+    }
+}
