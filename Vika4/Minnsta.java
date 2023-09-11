9f6900f5a5454da332b2cee7b74d4b289ef006fb
Ebba Þóra Hvannberg <ebba@hi.is>
2023-09-11 18:13:34 +0000

    Fyrsta útgáfa

---
+++ Vika4/Minnsta.java
@@ -0,0 +1,21 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *
+ *  Lýsing  : Les inn  inn þrjár heiltölur af console
+ *  og finnur minnstu töluna og prentar hana út
+ *
+ *****************************************************************************/
+
+public class Minnsta {
+    public static void main(String[] args) {
+        Scanner inntak = new Scanner(System.in, StandardCharsets.UTF_8);
+        int a = inntak.nextInt();
+        int b = inntak.nextInt();
+        int c = inntak.nextInt();
+        System.out.println(Math.min(a, Math.min(b, c)));
+    }
+}
