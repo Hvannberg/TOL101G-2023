da592f4ce4b4271969b9a0f06ce1ae15f3535940
Ebba Þóra Hvannberg <ebba@hi.is>
2023-09-11 21:58:29 +0000

    Fyrsta útgáfa

---
+++ Vika4/CharStafir.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *
+ *  Lýsing  :
+ *
+ *
+ *****************************************************************************/
+
+public class CharStafir {
+    public static void main(String[] args) {
+        System.out.println('b'); // b
+        System.out.println('b' + 'c'); // dec 98 + 99
+        System.out.println((char)('b'+'c'));
+        char c = 'a' + 4;
+        System.out.println(c); // e
+    }
+}
