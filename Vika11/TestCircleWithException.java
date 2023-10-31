9f1e130ed2f4ffb1eb0adf0cce7cfef2faee66a5
Ebba Þóra Hvannberg <ebba@hi.is>
2023-10-31 19:57:36 +0000

    Fyrsta útgáfa - Liang

---
+++ Vika11/TestCircleWithException.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Nafn    : Liang
+ *
+ *  Lýsing  : Client forrit fyrir CircleWithException
+ *
+ *
+ *****************************************************************************/
+
+public class TestCircleWithException {
+    public static void main(String[] args) {
+        try {
+            CircleWithException c1 = new CircleWithException(5);
+            CircleWithException c2 = new CircleWithException(-5);
+            CircleWithException c3 = new CircleWithException(0);
+        }
+        catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println("Number of objects created: " +
+                                   CircleWithException.getNumberOfObjects());
+    }
+}
