b20ffdb4fc060a86adc1d15ef4d66abe897425ac
Ebba Þóra Hvannberg <ebba@hi.is>
2023-11-01 11:13:46 +0000

    fjarlægði dead store

--- Vika11/TestCircleWithException.java
+++ Vika11/TestCircleWithException.java
@@ -9,9 +9,9 @@
 public class TestCircleWithException {
     public static void main(String[] args) {
         try {
-            CircleWithException c1 = new CircleWithException(5);
-            CircleWithException c2 = new CircleWithException(-5);
-            CircleWithException c3 = new CircleWithException(0);
+            new CircleWithException(5);
+            new CircleWithException(-5);
+            new CircleWithException(0);
         }
         catch (IllegalArgumentException ex) {
             System.out.println(ex.getMessage());
